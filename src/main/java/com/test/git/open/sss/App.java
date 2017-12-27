22d0978ee83aee5f1d87882f8980dec05c609deb
free_xinxin <121635629@qq.com>
2017-12-27 18:21:04 +0800

    Initial commit

---
+++ src/main/java/com/test/git/open/sss/App.java
@@ -0,0 +1,13 @@
+package com.test.git.open.sss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
