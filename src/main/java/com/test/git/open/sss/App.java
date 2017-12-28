92e2bc5623d4667b913276b755dd42cb625ab7be
free_xinxin <121635629@qq.com>
2017-12-28 22:15:24 +0800

    修改输出

--- src/main/java/com/test/git/open/sss/App.java
+++ src/main/java/com/test/git/open/sss/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      String aa = "1228分支修改";
+        System.out.println( "Hello World!  aasda" );
     }
 }
