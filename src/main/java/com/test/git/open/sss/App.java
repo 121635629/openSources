507ff92534a7c9da4cb1463db12afc67a8d1b088
free_xinxin <121635629@qq.com>
2017-12-28 22:31:54 +0800

    分支2

--- src/main/java/com/test/git/open/sss/App.java
+++ src/main/java/com/test/git/open/sss/App.java
@@ -4,10 +4,11 @@ package com.test.git.open.sss;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+          System.out.println( "gggggfff!" );
     }
 }
